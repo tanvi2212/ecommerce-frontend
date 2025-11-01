bc1e8b44f34f8f5227c48b9223407d8c3ec549ee
Tanvi Patil <tanvipatil2224@gmail.com>
2025-11-01 16:07:41 +0530

    Added Hero Section with responsive design and animations

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,19 @@
 console.log("E-Commerce Website Loaded");
 
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+document.querySelector(".cta-btn").addEventListener("click", () => {
+    window.scrollTo({
+        top: document.body.scrollHeight / 3,
+        behavior: "smooth"
+    });
+});
+
+
+
 
